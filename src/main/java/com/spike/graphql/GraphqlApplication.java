8e5c3d1e38204fc18a1135d69e6cbdea8f1ecb70
surverohan <rohansurve26@gmail.com>
2020-02-09 11:42:21 -0600

    Update GraphqlApplication.java

--- src/main/java/com/spike/graphql/GraphqlApplication.java
+++ src/main/java/com/spike/graphql/GraphqlApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GraphqlApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GraphqlApplication.class, args);
 	}
 
